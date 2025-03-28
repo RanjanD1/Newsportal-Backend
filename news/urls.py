0c2f4e3d80053a9d971985bfbf838cd9c7867921
Ranjan Dangol <163811087+RanjanD1@users.noreply.github.com>
2025-03-28 18:25:08 +0545

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserListView, ArticleListView, ArticleDetailView, CategoryListView, CommentListView, NewsClassificationView, NewsRecommendationView
+from .views import UserListView, ArticleListView, ArticleDetailView, CategoryListView, CommentListView, NewsClassificationView, NewsRecommendationView, login_view, signup_view, home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('users/', UserListView.as_view(), name='user-list'),
     path('articles/', ArticleListView.as_view(), name='article-list'),
     path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('comments/', CommentListView.as_view(), name='comment-list'),
     path('classify-news/', NewsClassificationView.as_view(), name='classify-news'),
     path('recommend-news/', NewsRecommendationView.as_view(), name='recommend-news'),
+
+    #Auth Pages
+    path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
 ]
